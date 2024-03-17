0f00876689468d124b9c5a92f91e28365da234bb
thijnmens <thijnsmulders04@gmail.com>
2024-03-17 19:31:49 +0100

    1 setup GitHub actions frontend (#4)
    * Google OAuth
    
    * Add actions
    
    * Fix Node build
    
    * Create docker setup
    
    * Fix nextjs build to work with docker

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,33 @@
-'use client'
 import Image from "next/image";
-import Button from "@/components/Button";
 
 export default function Login() {
-	
 	return (
 		<main className="h-screen">
-			<div className='flex justify-between h-full'>
-				<div className='p-16 w-full'>
-					<h1 className='text-6xl border-b-2 w-fit'>Login</h1>
-					<div className='m-16'>
-						<Button title='Sign in with google' />
+			<div className="flex justify-between h-full">
+				<div className="p-16 w-full">
+					<h1 className="text-6xl border-b-2 w-fit">Login</h1>
+					<div className="m-16">
+						<div className="flex">
+							<a
+								className="text-white mx-auto"
+								title="google"
+								href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/google`}
+							>
+								<div className="px-8 py-4 bg-blue-500 rounded-lg">
+									Login with google
+								</div>
+							</a>
+						</div>
 					</div>
 				</div>
-				<div className='w-1/2 max-h-screen relative'>
-					<Image className='rounded-bl-[300px] object-cover shadow-2xl' src='/koala.webp' alt='Login Banner' fill priority />
+				<div className="w-1/2 max-h-screen relative">
+					<Image
+						className="rounded-bl-[300px] object-cover shadow-2xl"
+						src="/koala.webp"
+						alt="Login Banner"
+						fill
+						priority
+					/>
 				</div>
 			</div>
 		</main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
 	return (
 		<main className="p-16">
 			<div>
-				<h1 className='text-8xl font-thin border-b-2 w-fit'>Vessem</h1>
-				<p className='p-2'>Learn languages today</p>
+				<h1 className="text-8xl font-thin border-b-2 w-fit">Vessem</h1>
+				<p className="p-2">Learn languages today</p>
 			</div>
 		</main>
 	);
